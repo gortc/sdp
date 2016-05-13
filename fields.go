be3108d89e39dacc6ab4dbe38636ec1c285b7bf2
Aleksandr Razumov <ar@cydev.ru>
2016-05-13 21:38:21 +0300

    fields: refactor append

--- fields.go
+++ fields.go
@@ -10,6 +10,28 @@ func appendSpace(v []byte) []byte {
 	return appendRune(v, ' ')
 }
 
+func appendJoin(b []byte, v [][]byte) []byte {
+	last := len(v) - 1
+	for i, vv := range v {
+		b = append(b, vv...)
+		if i != last {
+			b = appendSpace(b)
+		}
+	}
+	return b
+}
+
+func appendJoinStrings(b []byte, v ...string) []byte {
+	last := len(v) - 1
+	for i, vv := range v {
+		b = append(b, vv...)
+		if i != last {
+			b = appendSpace(b)
+		}
+	}
+	return b
+}
+
 func (s Session) AddVersion(v int) Session {
 	return s.append(TypeProtocolVersion, []byte(strconv.Itoa(v)))
 }
@@ -24,11 +46,7 @@ func (s Session) AddEmail(email string) Session {
 
 func (s Session) AddConnectionData(netType, addType, data string) Session {
 	v := make([]byte, 0, 256)
-	v = append(v, netType...)
-	v = appendSpace(v)
-	v = append(v, addType...)
-	v = appendSpace(v)
-	v = append(v, data...)
+	v = appendJoinStrings(v, netType, addType, data)
 	return s.append(TypeConnectionData, v)
 }
 
